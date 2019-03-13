e31c5f546160f8e33c51aaa14ce93df2c90b98ba
kauly <kauly@hotmail.com>
2019-03-13 15:01:45 -0300

    socket

--- src/pages/main.js
+++ src/pages/main.js
@@ -11,7 +11,7 @@ import Tweets from "../components/Tweets";
 import Follow from "../components/Follow";
 import Modal from "../components/Modal";
 
-const socket = io("http://localhost:5000");
+const socket = io(process.env.REACT_APP_API);
 
 const useStyles = makeStyles({
   container: {
